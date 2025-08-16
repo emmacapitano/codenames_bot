611c14c8d5770a66cb7c16a962b39d910e075f7d
Alex Pan <apan82@gmail.com>
2025-08-15 23:37:43 -0400

    Adding repel_word_vector

--- codemaster.py
+++ codemaster.py
@@ -62,8 +62,48 @@ def get_most_similar_word(g:GameState, word:str) -> str:
 
 
 def get_cluster_centroids(word_vectors:np.array, n_clusters=4) -> np.array:
+    """
+    Uses K-means to find centroids of unsupervised clusters
+    """
     k_means = KMeans(n_clusters=n_clusters, 
                     random_state=0, 
                     n_init="auto").fit(word_vectors)
     
     return(k_means.cluster_centers_)
+
+
+def repel_word_vector(g:GameState, team_color:str, num_clusters:int):
+    """
+    Moves word vectors further away other team's word vectors
+    Inspired by Coulomb's law to "repel" away from undesirable words
+    """
+    available_words = (g.assignment_board == team_color) * (g.covered_words == False) # 5x5 np.array of bools
+    available_wv = g.word_vectors[available_words] # n x g.wv.vector_size
+    
+    # Bad words are any other word
+    bad_words = (g.assignment_board != team_color) * (g.covered_words == False) # 5x5 np.array of bools
+    bad_words_assignment = g.assignment_board[bad_words]
+    bad_wv = g.word_vectors[bad_words] # m x g.wv.vector_size
+    
+    # Define the weight of the "repulsive" force
+    weights = {'r': 10, 'b': 10, 'a': 20, 'g': 5}
+
+    # Find unsupervised cluster centroids
+    cluster_centers_wv = get_cluster_centroids(word_vectors=available_wv, 
+                          n_clusters=num_clusters)
+
+    # Calculate the Coulomb repulsion forces from the bad words on the available words
+    # Weight the repulsive force by weights
+    f_repulse_12 = []
+    for r_2_i, bwa in zip(bad_wv, bad_words_assignment):
+        q = weights[bwa] # int
+        r_12 = cluster_centers_wv - r_2_i # num_clusters x 25
+        mag_repulse = np.linalg.norm(r_12, axis=1) 
+        mag_repulse = np.resize(mag_repulse, (mag_repulse.shape[0], 1)) # 25 x 1
+        repulse =  q * r_12 / (mag_repulse ** 3) # num_clusters x 25
+        f_repulse_12.append(repulse)
+    
+    f_repulse_12 = np.array(f_repulse_12) # num_other_ix x num_clusters x 25
+    f_repulse_12 = np.sum(f_repulse_12, axis=0) / f_repulse_12.shape[0] # num_clusters x 25
+
+    return cluster_centers_wv + f_repulse_12
